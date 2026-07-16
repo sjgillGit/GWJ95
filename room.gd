3249a9d931cab2fe2386d1e86972c60c70f0c26b
Saxinpoppy <exiong3712@gmail.com>
2026-07-15 23:03:00 -0400

    Map Complete, better cameras

--- room.gd
+++ room.gd
@@ -2,7 +2,7 @@ extends Node3D
 var x_camera_pos = position.x
 var z_camera_pos = position.z + 8
 
-@export var camera_pos = Vector3(0,0,0)
+var camera_pos = Vector3(position.x,20,position.z + 8)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:

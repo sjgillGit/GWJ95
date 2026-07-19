ab5c38c8eeb6eb673e7033ab6667ef276e3f7598
Dan <dwoj714@gmail.com>
2026-07-19 15:24:07 -0400

    Knight on main menu

--- knight_anim_controller.gd
+++ knight_anim_controller.gd
@@ -1,11 +1,16 @@
 extends Node3D
 
 @export var player_controller: CharacterBody3D
+@export var player_controlled: bool = true
+@export var anim_walking: bool = false
+
 
 var xz: Vector3
 
 func _process(delta):
-	xz = Vector3(player_controller.velocity.x, 0, player_controller.velocity.z)
-	if xz.length_squared() > 0.1:
-		look_at(global_position - xz)
-		
+	if player_controlled && player_controller != null:
+		xz = Vector3(player_controller.velocity.x, 0, player_controller.velocity.z)
+		anim_walking = xz.length_squared() > 0.1
+		if anim_walking:
+			look_at(global_position - xz)
+			

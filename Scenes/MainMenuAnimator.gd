086b50d296c2b876b397658361d7bed8a3d76a40
Dan <dwoj714@gmail.com>
2026-07-19 13:08:17 -0400

    Main Menu update
    Added torches, fixed fade-to-black on the camera

--- Scenes/MainMenuAnimator.gd
+++ Scenes/MainMenuAnimator.gd
@@ -1,6 +1,7 @@
 extends AnimationPlayer
 
 @export var menu2D: Control
+@export var menu3D: Node3D
 
 func _ready():
 	menu2D.game_starting.connect(on_game_starting)
@@ -8,3 +9,7 @@ func _ready():
 func on_game_starting():
 	menu2D.visible = false
 	play("stf_exterior/GameStartAnim")
+
+func on_start_anim_complete():
+	menu3D.visible = false
+	menu2D.begin_game()
